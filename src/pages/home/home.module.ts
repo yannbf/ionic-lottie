aa692590514f98f59a8e92e6d9bdc123c72476f7
Yann Braga <yannbf@gmail.com>
2017-04-19 12:49:21 -0300

    Update ng-lottie package

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,7 +1,7 @@
 import { HomePage } from './home';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { LottieAnimationViewModule } from 'lottie-angular2';
+import { LottieAnimationViewModule } from 'ng-lottie';
 
 @NgModule({
     declarations: [
